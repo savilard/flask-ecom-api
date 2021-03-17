18b46b4d405a80a5d303281a0a689f3ba689250b
savilard <vitaliyantonoff@gmail.com>
2021-03-17 13:17:24 +0600

    tests: add "test_product" fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,18 @@ import pytest
 from flask_ecom_api import create_app, db
 
 
+class Product(db.Model):
+    __tablename__ = 'test_product'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+
+
+@pytest.fixture(scope='module')
+def test_product():
+    return Product(name='cool_product')
+
+
 @pytest.fixture(scope='module')
 def test_app():
     app = create_app()
--- tests/test_models/test_db_connection.py
+++
@@ -1,9 +0,0 @@
-from flask_ecom_api.models.product import Product
-
-
-def test_db_create(test_database):
-    product = Product(name='test_product')
-    test_database.session.add(product)
-    test_database.session.commit()
-
-    assert Product.query.first()
