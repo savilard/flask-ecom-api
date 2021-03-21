d5dfd502152c73dd3b90f21b2b1a65e91873a5af
Vitaliy Antonov <60862933+savilard@users.noreply.github.com>
2021-03-21 11:35:33 +0600

    tests: get the JSON data from the response with get_json (#5)

--- tests/test_api/test_v1/test_products.py
+++ tests/test_api/test_v1/test_products.py
@@ -1,17 +1,14 @@
-import json
-
-
 def test_get_products(test_app):
     client = test_app.test_client()
     response = client.get('/api/v1/products')
-    data = json.loads(response.data.decode())
+    json_data = response.get_json()
     assert response.status_code == 200
-    assert 'success' in data['status']
+    assert 'success' in json_data['status']
 
 
 def test_get_product(test_app):
     client = test_app.test_client()
     response = client.get('/api/v1/products/1')
-    data = json.loads(response.data.decode())
+    json_data = response.get_json()
     assert response.status_code == 200
-    assert 'success' in data['status']
+    assert 'success' in json_data['status']
