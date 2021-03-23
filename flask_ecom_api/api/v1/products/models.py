3b53c288e780beacc4dea3ad011eb6e6cd1baa01
savilard <vitaliyantonoff@gmail.com>
2021-03-23 16:02:37 +0600

    feat(products): add Product model

--- flask_ecom_api/api/v1/products/models.py
+++ flask_ecom_api/api/v1/products/models.py
@@ -1,8 +1,29 @@
+from slugify import slugify
+
 from flask_ecom_api import db
 
 
 class Product(db.Model):
-    __tablename__ = 'product'
-
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80))
+    name = db.Column(
+        db.String(length=140),
+        index=True,
+        unique=True,
+        nullable=False,
+    )
+    slug = db.Column(db.String(length=140), unique=True, nullable=False)
+    description = db.Column(db.Text)
+    images = db.relationship('ProductImage', backref='product', lazy='joined')
+    price = db.Column(db.DECIMAL(10, 2), default=0)
+    published = db.Column(db.Boolean, default=False)
+
+    def __init__(self, *args, **kwargs):
+        super(Product, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def generate_slug(self):
+        if self.name:
+            self.slug = slugify(text=self.name, max_length=140)
+
+    def __repr__(self):
+        return f'<Product id: {self.id}, product name: {self.name}>'
