7aba4f9d918d30b7565e15c105a7ddb77b9a9357
savilard <vitaliyantonoff@gmail.com>
2021-03-23 16:17:53 +0600

    feat(products): add Ingredient model

--- flask_ecom_api/api/v1/products/models.py
+++ flask_ecom_api/api/v1/products/models.py
@@ -2,6 +2,22 @@ from slugify import slugify
 
 from flask_ecom_api import db
 
+product_ingredients = db.Table(
+    'product_ingredients',
+    db.Column(
+        'product_id',
+        db.Integer,
+        db.ForeignKey('product.id'),
+        primary_key=True,
+    ),
+    db.Column(
+        'ingredient_id',
+        db.Integer,
+        db.ForeignKey('ingredient.id'),
+        primary_key=True,
+    ),
+)
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,6 +32,13 @@ class Product(db.Model):
     images = db.relationship('ProductImage', backref='product', lazy='joined')
     price = db.Column(db.DECIMAL(10, 2), default=0)
     published = db.Column(db.Boolean, default=False)
+
+    ingredients = db.relationship(
+        'Ingredient',
+        secondary=product_ingredients,
+        lazy='subquery',
+        backref=db.backref('products', lazy='joined'),
+    )
 
     def __init__(self, *args, **kwargs):
         super(Product, self).__init__(*args, **kwargs)
@@ -42,3 +65,20 @@ class ProductImage(db.Model):
 
     def __repr__(self):
         return f'<Image id: {self.id}, image src: {self.src}>'
+
+
+class Ingredient(db.Model):
+    """Model for product ingredient."""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(
+        db.String(length=140),
+        index=True,
+        unique=True,
+        nullable=False,
+    )
+    image_src = db.Column(db.String(140))
+    weight = db.Column(db.Integer, default=0)
+    price = db.Column(db.DECIMAL(10, 2), default=0)
+
+    def __repr__(self):
+        return f'<Ingredient id: {self.id}, ingredient name: {self.name}>'
