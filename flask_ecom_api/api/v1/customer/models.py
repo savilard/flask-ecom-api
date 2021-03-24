8fa176f3e0521e33e1644f5dbcbedfbe6a4d5f14
savilard <vitaliyantonoff@gmail.com>
2021-03-24 11:00:34 +0600

    feat(customers): add CustomerShippingAddress model

--- flask_ecom_api/api/v1/customer/models.py
+++ flask_ecom_api/api/v1/customer/models.py
@@ -19,3 +19,26 @@ class Customer(db.Model):
 
     def __repr__(self):
         return f'<Customer id: {self.id}, customer name: {self.name}>'
+
+
+class CustomerShippingAddress(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(
+        db.Integer,
+        db.ForeignKey('customer.id'),
+        index=True,
+        nullable=False,
+    )
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    phone_number = db.Column(db.String(10), nullable=False)
+    country = db.Column(db.String(20), nullable=False)
+    city = db.Column(db.String(20), nullable=False)
+    street = db.Column(db.String(20), nullable=False)
+    house_number = db.Column(db.Integer, nullable=False)
+    apartment_number = db.Column(db.Integer, nullable=False)
+    postcode = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(140))
+
+    def __repr__(self):
+        return f'<Customer shipping address id: {self.id}>'
