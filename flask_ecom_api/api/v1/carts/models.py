9f9c352b5b28c5f31dc2f0ccf09a7d68b7dc9200
savilard <vitaliyantonoff@gmail.com>
2021-03-28 15:28:46 +0600

    feat(carts): add cart_product model

--- flask_ecom_api/api/v1/carts/models.py
+++ flask_ecom_api/api/v1/carts/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from flask_ecom_api import db
+from flask_ecom_api.api.v1.restaurants.models import RestaurantProduct
 
 
 class Cart(db.Model):
@@ -17,3 +18,30 @@ class Cart(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow())
     updated_at = db.Column(db.DateTime)
     expires_at = db.Column(db.DateTime)
+
+    products = db.relationship('CartProduct')
+
+
+class CartProduct(db.Model):
+    """Cart product model."""
+
+    id = db.Column(db.Integer, primary_key=True)
+    cart_id = db.Column(
+        db.Integer,
+        db.ForeignKey('cart.id'),
+        index=True,
+        nullable=False,
+    )
+    restaurant_product_id = db.Column(
+        db.Integer,
+        db.ForeignKey('restaurant_product.id'),
+        index=True,
+        nullable=False,
+    )
+    quantity = db.Column(db.Integer, default=0)
+
+    cart = db.relationship('Cart', lazy='joined')
+    restaurant_product = db.relationship(RestaurantProduct, lazy='joined')
+
+    def __repr__(self):
+        return f'<CartProduct id: {self.id}>'
