43fd90ba8ed6ca5d19acf1bf367ddbaf365a337a
savilard <vitaliyantonoff@gmail.com>
2021-04-02 15:58:04 +0600

    feat(config): add secret_key to BaseConfig

--- flask_ecom_api/config.py
+++ flask_ecom_api/config.py
@@ -6,6 +6,7 @@ class BaseConfig:
 
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'my_cool_secret_key')
 
 
 class DevelopmentConfig(BaseConfig):
