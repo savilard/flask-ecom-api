713353c12f1c2f02ccca0c450f73b0d35b3cd41c
savilard <vitaliyantonoff@gmail.com>
2021-04-03 11:53:16 +0600

    refactor(products): make flake8 happy to admin.py

--- flask_ecom_api/api/v1/products/admin.py
+++ flask_ecom_api/api/v1/products/admin.py
@@ -4,25 +4,33 @@ from flask_ecom_api.api.v1.common.custom_admin_views import ImageView
 
 
 class ProductAdminView(ModelView):
+    """Product admin view."""
+
     can_view_details = True
     column_display_pk = True
-    column_list = ('id', 'name', 'categories', 'price', 'slug',)
+    column_list = ('id', 'name', 'categories', 'price', 'slug')
     please_do_show_primary_keys_value = True
 
 
 class CategoryAdminView(ModelView):
+    """Category admin view."""
+
     can_view_details = True
-    column_list = ('id', 'name', 'slug', 'parent',)
+    column_list = ('id', 'name', 'slug', 'parent')
     please_do_show_primary_keys_value = True
 
 
 class IngredientAdminView(ModelView):
+    """Ingredient admin view."""
+
     can_view_details = True
-    column_list = ('id', 'name',)
+    column_list = ('id', 'name')
     please_do_show_primary_keys_value = True
 
 
 class ProductImageAdminView(ImageView):
+    """Product image admin view."""
+
     can_view_details = True
-    column_list = ('id', 'src', 'is_main', 'product', )
+    column_list = ('src', 'is_main', 'product')
     please_do_show_primary_keys_value = True
