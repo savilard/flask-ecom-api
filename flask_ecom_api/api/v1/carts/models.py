4b41b6093c551ba93ca07f876909b1bb59a19815
savilard <vitaliyantonoff@gmail.com>
2021-04-05 07:18:24 +0600

    feat(carts): check cart product quantity field non negative

--- flask_ecom_api/api/v1/carts/models.py
+++ flask_ecom_api/api/v1/carts/models.py
@@ -40,5 +40,10 @@ class CartProduct(db.Model):
     cart = db.relationship('Cart', lazy='joined')
     restaurant_product = db.relationship(RestaurantProduct, lazy='joined')
 
+    __table_args__ = (
+        db.CheckConstraint(quantity >= 0, name='check_quantity_non_negative'),
+        {},
+    )
+
     def __repr__(self):
         return f'<CartProduct id: {self.id}>'
