5150d6075397aa10bb980dd681b03e27d55c6364
savilard <vitaliyantonoff@gmail.com>
2021-04-07 15:09:39 +0600

    feat(products): add product_detail endpoint

--- flask_ecom_api/api/v1/products/views.py
+++ flask_ecom_api/api/v1/products/views.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, jsonify
 
 from flask_ecom_api import Product  # type: ignore
-from flask_ecom_api.api.v1.products.schemas import products_schema
+from flask_ecom_api.api.v1.products.schemas import (
+    product_schema,
+    products_schema,
+)
 
 product_blueprint = Blueprint('products', __name__)
 
@@ -11,4 +14,12 @@ def products():
     """Gets all products from db."""
     all_products = Product.query.all()
     output = products_schema.dump(all_products)
+    return jsonify({'data': output}), 200
+
+
+@product_blueprint.route('/products/<product_id>', methods=['GET'])
+def product_detail(product_id):
+    """Get product detail."""
+    product = Product.query.get(product_id)
+    output = product_schema.dump(product)
     return jsonify({'data': output}), 200
