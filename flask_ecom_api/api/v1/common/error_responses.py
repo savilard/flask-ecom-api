6f6ac58bb24853aa64d72e74c88b8fa353188596
savilard <vitaliyantonoff@gmail.com>
2021-04-10 10:24:29 +0600

    refactor(products): making error response moved to separate func

--- flask_ecom_api/api/v1/common/error_responses.py
+++ flask_ecom_api/api/v1/common/error_responses.py
@@ -1,27 +1,58 @@
+import uuid
 from dataclasses import dataclass
+from typing import Dict, Optional, Union
+
+from flask import jsonify
 
 
 @dataclass
-class ErrorResponse:  # noqa: WPS306
-    """Error response dataclass."""
+class HttpError:
+    """Http error dataclass."""
 
-    status: int
-    message: str
-    detail: str
+    status_code: int
+    message: Optional[str]
+    detail: Optional[str]
+    request_id: str = uuid.uuid4().hex
 
-    def construct_error_response(self):
-        """Return error response in dict.
-
-        :return: error response
-        :type: Dict[str, List[Dict[str, Union[int, str]]
-
-        """
+    def to_dict(self) -> Dict[str, Union[int, str, None]]:
+        """Convert HttpError to dict."""
         return {
-            'errors': [
-                {
-                    'status': self.status,
-                    'message': self.message,
-                    'detail': self.detail,
-                },
-            ],
+            'status': self.status_code,
+            'message': self.message,
+            'detail': self.detail,
+            'request_id': self.request_id,
         }
+
+
+def make_error_response(
+    status_code: int,
+    message: Optional[str] = None,
+    detail: Optional[str] = None,
+):
+    """Returns an error response.
+
+    If status code = 500, message and detailed message do not need to be transmitted.
+
+    :param status_code: error status code
+    :param message: error text
+    :param detail: error detail text
+    :return: error response and status code
+    """
+    errors = {
+        500: HttpError(
+            status_code=500,  # noqa: WPS432
+            message='Internal Server Error',
+            detail='There was an internal server error',
+        ).to_dict(),
+    }
+
+    error = errors.get(
+        status_code,
+        HttpError(
+            status_code=status_code,
+            message=message,
+            detail=detail,
+        ).to_dict(),
+    )
+
+    return jsonify({'errors': [error]}), error['status']
--- flask_ecom_api/api/v1/products/views.py
+++ flask_ecom_api/api/v1/products/views.py
@@ -3,7 +3,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from webargs.flaskparser import use_args
 
 from flask_ecom_api import Product, ProductImage  # type: ignore
-from flask_ecom_api.api.v1.common.error_responses import ErrorResponse
+from flask_ecom_api.api.v1.common.error_responses import make_error_response
 from flask_ecom_api.api.v1.products.schemas import (
     product_image_schema,
     product_schema,
@@ -20,12 +20,7 @@ def get_all_products():
     try:
         all_products = Product.query.all()
     except SQLAlchemyError:
-        response = ErrorResponse(
-            status=500,  # noqa: WPS432
-            message='Internal Server Error',
-            detail='There was an internal server error',
-        ).construct_error_response()
-        return jsonify(response), 500
+        return make_error_response(status_code=500)
 
     response = {'data': products_schema.dump(all_products)}
     return jsonify(response), 200
@@ -47,12 +42,7 @@ def create_product(args):
         db.session.commit()
     except SQLAlchemyError:
         db.session.rollback()
-        response = ErrorResponse(
-            status=500,  # noqa: WPS432
-            message='Internal Server Error',
-            detail=f'The product named "{new_product_name}" is not created',
-        ).construct_error_response()
-        return jsonify(response), 500
+        return make_error_response(status_code=500)
 
     response = {'data': product_schema.dump(new_product)}
     return jsonify(response), 201
@@ -64,20 +54,14 @@ def product_detail(product_id):
     try:
         product = Product.query.filter_by(id=product_id).first()
     except SQLAlchemyError:
-        response = ErrorResponse(
-            status=500,  # noqa: WPS432
-            message='Internal Server Error',
-            detail='There was an internal server error',
-        ).construct_error_response()
-        return jsonify(response), 500
+        return make_error_response(status_code=500)
 
     if not product:
-        response = ErrorResponse(
-            status=404,  # noqa: WPS432
+        return make_error_response(
+            status_code=404,
             message='Product not found',
-            detail='The requested product could not be found',
-        ).construct_error_response()
-        return jsonify(response), 404
+            detail=f'The product with id={product_id} could not be found',
+        )
 
     response = {'data': product_schema.dump(product)}
     return jsonify(response), 200
@@ -95,12 +79,7 @@ def create_product_image(args):
         db.session.commit()
     except SQLAlchemyError:
         db.session.rollback()
-        response = ErrorResponse(
-            status=500,  # noqa: WPS432
-            message='Internal Server Error',
-            detail=f'The image with src "{new_product_image}" is not created',
-        ).construct_error_response()
-        return jsonify(response), 500
+        return make_error_response(status_code=500)
 
     response = {'data': product_image_schema.dump(new_product_image)}
     return jsonify(response), 201
