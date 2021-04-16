1178d3918dc4805eb300a458c9e33aa14d0ff367
savilard <vitaliyantonoff@gmail.com>
2021-04-16 16:19:48 +0600

    refactor(users): make role field as ENUM in user model

--- flask_ecom_api/api/v1/users/models.py
+++ flask_ecom_api/api/v1/users/models.py
@@ -1,10 +1,19 @@
 import datetime
+import enum
 
 from sqlalchemy_utils import EmailType
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from flask_ecom_api.api.v1.users.admin import UserAdminView
 from flask_ecom_api.app import admin, db
+
+
+class UserRoleEnum(enum.Enum):
+    """Class for choosing user role."""
+
+    guest = 'guest'
+    admin = 'admin'
+    manager = 'manager'
 
 
 class User(db.Model):
@@ -22,11 +31,10 @@ class User(db.Model):
     password = db.Column(db.String(128))
     active = db.Column(db.Boolean(), default=True, nullable=False)
     created_date = db.Column(db.DateTime, default=datetime.datetime.now())
-
-    roles = db.relationship(
-        'Role',
-        secondary='user_role',
-        lazy='joined',
+    role = db.Column(
+        db.Enum(UserRoleEnum),
+        default=UserRoleEnum.guest,
+        nullable=False,
     )
 
     def set_password(self, password):  # noqa: WPS615
@@ -40,44 +48,6 @@ class User(db.Model):
     def __repr__(self):
         """Printable representation of Users model."""
         return self.email
-
-
-class Role(db.Model):
-    """Role model."""
-
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True, index=True, nullable=False)
-    description = db.Column(db.String(255))
-
-    def __repr__(self):
-        """Printable representation of Role model."""
-        return self.name
-
-
-class UserRole(db.Model):
-    """Roles users model."""
-
-    id = db.Column(db.Integer, primary_key=True)
-
-    user_id = db.Column(
-        db.Integer(),
-        db.ForeignKey('user.id'),
-        index=True,
-        nullable=False,
-    )
-    role_id = db.Column(
-        db.Integer(),
-        db.ForeignKey('role.id'),
-        index=True,
-        nullable=False,
-    )
-
-    user = db.relationship('User', lazy='joined')
-    role = db.relationship('Role', lazy='joined')
-
-    def __repr__(self):
-        """Printable representation of UserRole model."""
-        return f'<{self.user_id} - {self.role_id}>'
 
 
 admin.add_view(UserAdminView(User, db.session, category='Users'))
